d8c4417d9cb6b4b2c070ed975b2e730bd46ec66d
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-06-03 12:12:21 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/ArrowDraw.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowDraw : MonoBehaviour
+{
+    [SerializeField]
+    private Image arrowImage;
+    private Vector3 crickPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private Vector3 clickPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            clickPosition = Input.mousePosition;
+            arrowImage.gameObject.SetActive(true);
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 dist=clickPosition-Input.mousePosition;
+
+            float size =dist.magnitude;
+
+            float angleRad = Mathf.Atan2(dist.y, dist.x);
+
+            arrowImage.rectTransform .position = clickPosition;
+
+            arrowImage.rectTransform.rotation 
+                = Quaternion.Euler(0, 0, angleRad * Mathf.Rad2Deg);
+
+            arrowImage.rectTransform.sizeDelta=new Vector2 (size, size);
+
+            arrowImage.gameObject.SetActive(false);
+
+            Debug.Log(dist);
+        }
+    }
+}
